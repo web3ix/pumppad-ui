e4a3d4549e594dc64ce09b1b21a192440dc147a3
noobmdev <noobmdev@gmail.com>
2024-09-07 20:10:50 +0700

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import TopBar from "@/components/home/Topbar";
 import Image from "next/image";
 
 const CHAINS_SUPPORTED = [
@@ -16,23 +15,21 @@ const CHAINS_SUPPORTED = [
 
 export default function CreateToken() {
     return (
-        <div className="px-[20px] flex flex-col items-stretch md:items-center gap-12 relative">
+        <div className="px-[20px] md:px-[100px] flex flex-col items-stretch md:items-center gap-12 relative">
             <div className="launch-bg1"></div>
-            <div className="flex flex-col gap-6 items-center">
-                <h1 className="text-[30px] leading-[160px] md:text-[200px] primary-text-gradient text-center mt-[60px]">
+            <div className="flex flex-col gap-4 items-center">
+                <h1 className="text-[32px] md:leading-[160px] md:text-[200px] primary-text-gradient text-center mt-[60px]">
                     PUMPPAD
                 </h1>
-                <div className="text-center">
-                    <div className="text-[20px] text-[#94A3B8] mb-2">
+                <div className="text-center text-[15px] md:text-[20px] text-[#94A3B8]">
+                    <div className="mb-2">
                         From Building to Launching in just a few steps
                     </div>
-                    <div className="text-[20px] text-[#94A3B8]">
-                        Powered by DEVHUB AI
-                    </div>
+                    <div>Powered by DEVHUB AI</div>
                 </div>
                 <div className="flex flex-col gap-5">
-                    <div className="flex justify-center gap-5">
-                        <button className="font-vortex md:block py-2 px-6 rounded-md text-sm font-semibold md:bg-[#0038FF]">
+                    <div className="flex flex-col md:flex-row justify-center gap-5">
+                        <button className="font-vortex md:block py-2 px-6 rounded-md text-sm font-semibold bg-[#0038FF]">
                             Connect wallet
                         </button>
 
@@ -40,7 +37,7 @@ export default function CreateToken() {
                             Launch your token now
                         </button>
 
-                        <button className="font-vortex md:block py-2 px-6 rounded-md text-sm font-semibold md:bg-[#0038FF]">
+                        <button className="font-vortex md:block py-2 px-6 rounded-md text-sm font-semibold bg-[#0038FF]">
                             Product order
                         </button>
                     </div>
@@ -93,6 +90,74 @@ export default function CreateToken() {
             </div>
 
             {/* <TopBar /> */}
+
+            <div>
+                <div className="flex flex-col md:flex-row p-7 gap-7 border-gradient rounded-xl">
+                    <div className="cursor-pointer w-full pt-[150%] md:w-[280px] md:pt-[400px] relative">
+                        <Image
+                            src="/mock-token.png"
+                            alt="mock-token"
+                            fill
+                            sizes="any"
+                        />
+                    </div>
+
+                    <div className="flex flex-col justify-between gap-10">
+                        <h1 className="leading-[100px] text-[100px]">DOGS</h1>
+                        <div>
+                            <div className="text-[#19FB9B] text-[14px] md:text-[20px]">
+                                Contract: TX9mipnPM6rHdpxpHmVqEn1bo9qY1u6tq3
+                            </div>
+                            {/* <div>icon</div> */}
+                        </div>
+
+                        <p className="text-[#94A3B8]">
+                            Who is Spotty? 🐕 Spotty, the unofficial logo and
+                            main mascot of VK, was created by Pavel Durov. He
+                            drew this iconic dog during a charity auction to
+                            support orphanages, and soon, Spotty became a
+                            beloved symbol of VK.
+                        </p>
+
+                        <div>
+                            <button className="font-vortex md:block py-2 px-6 rounded-md text-sm font-semibold bg-[#0038FF]">
+                                Pump it
+                            </button>
+                        </div>
+                        <div className="font-vortex grid grid-cols-1 md:grid-cols-4 gap-10">
+                            <div className="flex flex-col items-stretch justify-between gap-6 p-4 bg-[#000000] box-shadow-stats border-gradient rounded-xl">
+                                <div className="flex justify-between text-[16px]">
+                                    <div className="text-[#666666]">PRICE</div>
+                                    <div>15 %</div>
+                                </div>
+                                <div className="flex justify-between text-[32px]">
+                                    <div>0.00015</div>
+                                    <div>ETH</div>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col items-stretch justify-between gap-6 p-4 bg-[#000000] box-shadow-stats border-gradient rounded-xl">
+                                <div className="text-[16px] text-[#666666]">
+                                    Marketcap
+                                </div>
+                                <div className="text-[32px]">$100K</div>
+                            </div>
+                            <div className="flex flex-col items-stretch justify-between gap-6 p-4 bg-[#000000] box-shadow-stats border-gradient rounded-xl">
+                                <div className="text-[16px] text-[#666666]">
+                                    Liquidity
+                                </div>
+                                <div className="text-[32px]">$14k</div>
+                            </div>
+                            <div className="flex flex-col items-stretch justify-between gap-6 p-4 bg-[#000000] box-shadow-stats border-gradient rounded-xl">
+                                <div className="text-[16px] text-[#666666]">
+                                    Token created
+                                </div>
+                                <div className="text-[32px]">1H 17M</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
